910b301a46a3078150d34c835cb8fff536b4793d
nikmmd <nikmmd@gmail.com>
2023-08-12 04:25:07 -0500

    chore: restore test

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,7 +2,7 @@ import { adder, concatenate } from './index';
 
 describe('adder module', () => {
     test('test_adder', ()=> {
-        expect(adder(1,2)).toBe(4)
+        expect(adder(1,2)).toBe(3)
     });
 });
 
